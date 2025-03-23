4db6cd1b8a56f8fb60ec599cd91e98a12f5589d2
chiomavera <you@example.com>
2025-03-23 05:02:25 +0100

    feat: create wallet components

---
+++ src/app/components/WalletType.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+interface WalletTypeProps {
+  walletType: string;
+  balance: string;
+  imageUrl: string;
+}
+
+function WalletType({ walletType, balance, imageUrl }: WalletTypeProps) {
+  return (
+    <div className="flex w-[282px] h-20  gap-2 p-4  items-center rounded-lg border border- [#8477FF]">
+         <Image src={imageUrl} alt="A country flag of the currency in use" width={40} height={40} />
+      <div className="flex flex-col">
+        <span className="text-base leading-6 tracking-[-0.64px]">
+          {walletType}
+        </span>
+        <span className="text-xs text-[#C1C1C1] font-medium tracking-[-0.24px]">
+          {balance}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default WalletType;
