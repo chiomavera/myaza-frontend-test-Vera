ae1c99a4166568def78060498d9f410d14a1b26d
chiomavera <you@example.com>
2025-03-24 04:00:35 +0100

    Create a transactions1 page

---
+++ src/app/dashboard/transactions1.ts
@@ -0,0 +1,39 @@
+ const transactions1 = [
+    {
+      Name: "Adobe After Effect",
+      Date: "Sat, 20 Apr 2020",
+      Amount: "$80.09",
+      Status: "Deposited",
+     imageURL:  "/Adobe.svg"
+    },
+    {
+      Name: "McDonald's",
+      Date: "Sat, 20 Apr 2020",
+      Amount: "$80.09",
+      Status: "Deposited",
+     imageURL:  "/McDonald.svg"
+    },
+    {
+      Name: "Levi",
+      Date: "Sat, 20 Apr 2020",
+      Amount: "$80.09",
+      Status: "Deposited",
+     imageURL:  "/Levi.svg"
+    },
+    {
+      Name: "Adobe After",
+      Date: "Sat, 20 Apr 2020",
+      Amount: "$80.09",
+      Status: "Deposited",
+     imageURL:  "/Adobe.svg"
+    },
+    {
+      Name: "Levi",
+      Date: "Sat, 20 Apr 2020",
+      Amount: "$80.09",
+      Status: "Deposited",
+     imageURL:  "/Levi.svg"
+    },
+  ];
+
+  export default transactions1;
