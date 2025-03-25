ddf4a1b6624967190e5eda957ae02c1955e79847
chiomavera <you@example.com>
2025-03-25 11:10:42 +0100

    refactor: defined state for active selected card

--- src/app/components/ATMCard.tsx
+++ src/app/components/ATMCard.tsx
@@ -1,14 +1,19 @@
+import Image from "next/image";
 
-function ATMCard() {
+const cardImages = ["/ATMCard.svg", "/ATMCard.svg", "/ATMCard.svg"]; // Array of card images
+
+function ATMCard({ activeCard }: { activeCard: number }) {
   return (
-    <div className="relative bg-[image:var(--bg-card)] bg-cover bg-center w-full h-[200px]">
-      <span className="absolute top-14 mx-9 text-[28px] font-medium">$5,750,20</span>
-      <div className="absolute top-36 mx-9">
-        <span className="mr-29">5282 3456 7890 1289</span>
-        <span>09/25</span>
-      </div>
+    <div className="w-full">
+      <Image
+        src={cardImages[activeCard]} // Dynamically change card image
+        alt="ATM Card"
+        width={391}
+        height={196}
+        className="rounded-[20px] transition-all duration-300 ease-in-out"
+      />
     </div>
-  )
+  );
 }
 
-export default ATMCard
+export default ATMCard;
