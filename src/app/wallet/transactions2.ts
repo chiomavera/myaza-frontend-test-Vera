26f39777b05b7bce5630adc54db86d390b23826b
chiomavera <you@example.com>
2025-03-25 11:30:04 +0100

    feat: create transactions 2 details

---
+++ src/app/wallet/transactions2.ts
@@ -0,0 +1,36 @@
+const transactions2 = [
+  {
+    imageURL: "/Adobe.svg",
+    Name: "Adobe After Effect",
+    Date: "Sat, 20 Apr 2020",
+    Description: "Adobe after Virtual Card top-up",
+    Amount: "$80.09",
+    Status: "Deposited",
+  },
+  {
+    imageURL: "/Adobe.svg",
+    Name: "Adobe After Effect",
+    Date: "Sat, 20 Apr 2020",
+    Description: "Adobe after Virtual Card top-up",
+    Amount: "$80.09",
+    Status: "Deposited",
+  },
+  {
+    imageURL: "/Adobe.svg",
+    Name: "Adobe After Effect",
+    Date: "Sat, 20 Apr 2020",
+    Description: "Adobe after Virtual Card top-up",
+    Amount: "$80.09",
+    Status: "Deposited",
+  },
+  {
+    imageURL: "/Adobe.svg",
+    Name: "Adobe After Effect",
+    Date: "Sat, 20 Apr 2020",
+    Description: "Adobe after Virtual Card top-up",
+    Amount: "$80.09",
+    Status: "Deposited",
+  },
+];
+
+export default transactions2;
