ebcc2b54865c1cd3c23e9039b3435dcf63ed9d86
chiomavera <you@example.com>
2025-03-25 11:22:02 +0100

    refactor: ehance user interaction

--- src/app/components/Income.tsx
+++ src/app/components/Income.tsx
@@ -8,19 +8,34 @@ interface IncomeProps {
   customClass?: string;
 }
 
-function Income({title, price, imageUrl, priceCount, customClass }: IncomeProps) {
+function Income({
+  title,
+  price,
+  imageUrl,
+  priceCount,
+  customClass,
+}: IncomeProps) {
   return (
-    <div className="w-full flex p-4 justify-between align-center rounded-2xl bg-foreground">
-        <div className="flex gap-3">
-          <Image src={imageUrl} alt="icon" width={40} height={40} />
-            <div className="flex flex-col">
-                <span className="text-[#8C89B4] text-sm">{title}</span>
-                <span className="text-2xl font-semibold">{price}</span>
-            </div>
+    <div
+      className="w-full flex p-4 justify-between items-baseline rounded-2xl bg-foreground gap-2 
+                 hover:scale-105 transition-transform duration-300 ease-in-out shadow-md"
+    >
+      <div className="flex items-center gap-3">
+        <Image src={imageUrl} alt={`${title} icon`} width={40} height={40} />
+        <div className="flex flex-col">
+          <span className="text-[#8C89B4] text-sm">{title}</span>
+          <span className="text-2xl font-semibold">{price}</span>
         </div>
-        <span className={`${customClass} flex w-12 h-5 justify-center items-center p-1  rounded-[10px] text-xs`}>{priceCount}</span>
+      </div>
+      <span
+        className={`flex w-12 h-5 justify-center items-baseline p-1 rounded-[10px] text-xs transition-opacity duration-200 hover:opacity-80 ${
+          customClass || ""
+        }`}
+      >
+        {priceCount}
+      </span>
     </div>
-  )
+  );
 }
 
-export default Income
+export default Income;
