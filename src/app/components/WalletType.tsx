705f3a178c0c047f068d99bbf3fade1e9cb4634e
chiomavera <you@example.com>
2025-03-25 11:28:22 +0100

    refactor: ehance user interactions

--- src/app/components/WalletType.tsx
+++ src/app/components/WalletType.tsx
@@ -8,8 +8,13 @@ interface WalletTypeProps {
 
 function WalletType({ walletType, balance, imageUrl }: WalletTypeProps) {
   return (
-    <div className="flex w-[282px] h-20  gap-2 p-4  items-center rounded-lg border border- [#8477FF]">
-         <Image src={imageUrl} alt="A country flag of the currency in use" width={40} height={40} />
+    <div className="flex w-full h-20  gap-2 p-4  items-center rounded-lg border border- [#8477FF] cursor-pointer hover:shadow-lg active:scale-95 transition-all duration-200">
+      <Image
+        src={imageUrl}
+        alt="A country flag of the currency in use"
+        width={40}
+        height={40}
+      />
       <div className="flex flex-col">
         <span className="text-base leading-6 tracking-[-0.64px]">
           {walletType}
